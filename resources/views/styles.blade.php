4c0e230534d899695ded977caf965809e4a06f3e
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-09-10 16:57:39 -0500

    Quick Commit

--- resources/views/styles.blade.php
+++ resources/views/styles.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="{{ TKPM::asset('css/vendor.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ TKPM::asset(elixir('css/vendor.css')) }}">
 @if(!App::isLocal())
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 @else
