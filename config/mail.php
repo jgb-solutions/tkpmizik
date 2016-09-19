35c1e0671e5dfff7fe0c0b850aad84edb0608ddf
Jean Gerard Bousiquot <jgbneatdesign@gmail.com>
2016-09-19 10:19:39 -0500

    fixed the feeds

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => config('site.email'), 'name' => config('site.name')],
 
     /*
     |--------------------------------------------------------------------------
