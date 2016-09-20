074e4c0f3a271c7a1a8cd7938b4a7dfb220d0237
Jean Gerard Bousiquot <jgbneatdesign@gmail.com>
2016-09-19 21:12:16 -0500

    Updated the audio player link to https

--- resources/views/music/audio-player.blade.php
+++ resources/views/music/audio-player.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="col-sm-12 audio-content">
 		<div class="ui360 ui360-vis">
-			<a href="{{ route('music.play', $music->id) }}" type="audio/mpeg"></a>
+			<a href="{{ TKPM::route('music.play', $music->id) }}" type="audio/mpeg"></a>
 		</div>
 	</div>
 </div>
