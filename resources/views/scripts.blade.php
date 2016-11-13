cf6c50ec8559844934e3f00aff2fae77294430ad
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-11-13 04:01:12 -0500

    Updated the scripts file

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,16 +1,18 @@
-@if ( App::isLocal() )
-	<script src="{{ TKPM::asset(elixir('js/vendor.js')) }}"></script>
-@else
-	@include('inc.fb-script')
+{{--@if (App::isLocal())--}}
+    {{-- <script src="{{ TKPM::asset(elixir('js/vendor.js')) }}"></script> --}}
+{{-- @else --}}
+    {{--@include('inc.fb-script')--}}
     {{-- Google Analytics --}}
-	{{-- @include('inc.ga') --}}
+    {{-- @include('inc.ga') --}}
 
-	<!-- Latest compiled and minified CSS & JS -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified CSS & JS -->
+{{--     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script> --}}
+
     {{-- <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script> --}}
     {{-- <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min.js"></script> --}}
-    <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.0/angular.min.js"></script>
+
+{{--     <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.0/angular.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.0/angular-animate.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular-loading-bar/0.8.0/loading-bar.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/soundmanager2/2.97a.20150601/script/soundmanager2-nodebug-jsmin.js"></script>
@@ -20,8 +22,13 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/danialfarid-angular-file-upload/12.2.9/ng-file-upload.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jplayer/2.9.2/jplayer/jquery.jplayer.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jplayer/2.9.2/add-on/jplayer.playlist.min.js"></script>
+@endif --}}
+
+@if  (! App::isLocal())
+    @include('inc.fb-script')
 @endif
 
+<script src="{{ TKPM::asset(elixir('js/vendor.js')) }}"></script>
 <script src="{{ TKPM::asset(elixir('js/app.js')) }}"></script>
 
 @if (! Request::hasCookie('facebook-modal'))
