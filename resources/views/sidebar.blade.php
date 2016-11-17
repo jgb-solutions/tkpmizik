b848e243c68c4777d574b8ac90357a583c755974
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-11-17 03:35:19 -0500

    Added Google Ads

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,13 +1,13 @@
 <div class="col-sm-4 padding1" id="sidebar">
 
+	@include('inc.ads.sidebar')
+
+	<br>
+
 	@include('inc.cta-buttons')
 
 	<br>
 
 	@include('inc.modules.facebook')
-
-	<br>
-
-	@include('inc.ads.sidebar')
 
 </div>
